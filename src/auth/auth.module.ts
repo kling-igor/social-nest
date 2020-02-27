93caa46d879986eb9f4cdec178a214ef4c83ff9a
Igor Kling <klingiv@altarix.ru>
2020-02-27 12:01:55 +0400

    Provide ConfigService to JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-// import { UsersService } from '../user/users.service';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
@@ -15,10 +14,20 @@ import { LocalStrategy } from './local.strategy';
     ConfigModule,
     UserModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secretOrPrivateKey: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '60s',
+        },
+      }),
+      inject: [ConfigService],
     }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: { expiresIn: '60s' },
+    // }),
   ],
   providers: [AuthService, JwtStrategy, LocalStrategy],
   controllers: [AuthController],
